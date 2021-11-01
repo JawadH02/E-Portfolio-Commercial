f1a7ca247692245cdf01d449248e7d0bef3a6eae
JawadHanif <JawadHanif.002@gmail.com>
2021-11-01 14:46:34 -0400

    added fade-in

---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        } 
+    }
+}
